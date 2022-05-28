764ab968cc653245d356d0e9a0df1ba7ae9c0df0
lint707 <lint@yandex.ru>
2022-05-28 08:02:23 +0000

    new scr.py

---
+++ Homework/scripts/scr2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+bash_command = ["cd ~/devops-netology-1/Homework", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+is_change = False
+for result in result_os.split('\n'):
+  if result.find('modified') != -1:
+    prepare_result = result.replace('\tmodified:   ', '')
+    print(prepare_result)
+    break
+  else:
+    print('Wrong?')
+    break
+
