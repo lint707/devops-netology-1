756afc3b7da54df67545b451853a80244d315e5e
lint707 <lint@yandex.ru>
2022-05-28 08:14:26 +0000

    new scr.py-2

--- Homework/scripts/scr2.py
+++ Homework/scripts/scr2.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import os
-bash_command = ["cd ~/devops-netology-1/Homework", "git status"]
+bash_command = ["cd ~/devops-netology-1", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
-is_change = False
+#is_change = False
 for result in result_os.split('\n'):
   if result.find('modified') != -1:
     prepare_result = result.replace('\tmodified:   ', '')
     print(prepare_result)
-    break
-  else:
-    print('Wrong?')
-    break
+#    break
+#  else:
+#    print('Wrong?')
+#    break
 
